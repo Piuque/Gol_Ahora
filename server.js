aa736d6f2d06e9c3be9c13f725621d37c76ee829
Piuque Ailin Carteau <piuqueailincarteau@gmail.com>
2026-06-07 21:32:08 -0300

    fix: configurar credentials en cors y agregar SweetAlert para errores de carga de perfil

--- server.js
+++ server.js
@@ -32,7 +32,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configurar Middlewares globales
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/css', express.static(path.join(__dirname, 'public/pages/css')));
